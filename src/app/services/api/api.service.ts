f56a28764cc91e35694f55c2c5d7abb9d33eff97
aron211 <aronsandoval16@gmail.com>
2023-02-15 15:14:38 -0400

    edit and delete ready

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -32,4 +32,18 @@ export class ApiService {
     let direccion = this.url + "pacientes?id=" + id;
     return this.http.get<pacienteI>(direccion);
   }
+  putPatient(form:pacienteI):Observable<ResponseI>{
+    let direccion = this.url + "pacientes";
+    return this.http.put<ResponseI>(direccion, form);
+  }
+  delete(form:pacienteI):Observable<ResponseI>{
+    let direccion = this.url + "pacientes"; 
+    let Options = {
+      headers: new HttpHeaders({
+      'content-Type': 'application/json'
+    }),
+    body:form
+  }
+    return this.http.delete<ResponseI>(direccion, Options)
+  }
 }
--- src/app/vistas/edit/edit.component.ts
+++ src/app/vistas/edit/edit.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute} from '@angular/router'
 import { ApiService } from '../../services/api/api.service'
 import { pacienteI } from '../../modelos/paciente.interface'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-edit',
@@ -13,12 +14,41 @@ export class EditComponent implements OnInit {
 
   constructor(private api:ApiService, private activerouter:ActivatedRoute, private router:Router) { }
 
+  datosPacientes:pacienteI | undefined;
+  editForm = new FormGroup ({
+    pacienteId :  new FormControl(''), 
+    dni : new FormControl(''),
+    nombre : new FormControl(''),
+    direccion : new FormControl(''),
+    codigoPostal : new FormControl(''),
+    telefono :  new FormControl(''),
+    genero : new FormControl(''),
+    fechaNacimiento :  new FormControl(''),
+    correo : new FormControl(''),
+    token :  new FormControl(''),
+
+  })
 
   ngOnInit(): void {
     let pacienteid = this.activerouter.snapshot.paramMap.get('id')!;
     let token = this.getToken();
     this.api.getPatient(pacienteid).subscribe(data => {
-      console.log(data)
+      this.datosPacientes = data[0]
+      console.log(this.datosPacientes)
+      this.editForm.setValue({
+        'pacienteId' : pacienteid,
+        'dni' : this.datosPacientes?.DNI!,
+        'nombre' : this.datosPacientes?.Nombre!, 
+        // 'nombre': this.datosPacientes[0].Nombre
+        'direccion' : this.datosPacientes?.Direccion!,
+        'codigoPostal' : this.datosPacientes?.CodigoPostal!,
+        'telefono' : this.datosPacientes?.Telefono!,
+        'genero' : this.datosPacientes?.Genero!,
+        'fechaNacimiento' : this.datosPacientes?.FechaNacimiento!,
+        'correo' : this.datosPacientes?.Correo!,
+        'token' : token,
+      })
+      console.log(this.editForm.value)
     })
     // console.log(pacienteid)
     // console.log(token)
@@ -26,6 +56,24 @@ export class EditComponent implements OnInit {
 
   getToken(){
     return localStorage.getItem('token')
+  }
+
+  postForm(form: any){
+    this.api.putPatient(form).subscribe(data =>{
+        console.log(data)
+        this.close();
+    })
+  }
+  delete(){
+    // console.log('eliminado')
+    let datos:any = this.editForm.value;
+    this.api.delete(datos).subscribe(data =>{
+      console.log(data)
+      this.close();
+    });
+  }
+  close(){
+    this.router.navigate(['dashboard'])
   }
 
 }
