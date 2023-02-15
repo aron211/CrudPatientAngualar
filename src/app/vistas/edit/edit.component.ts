4be81a4b480664c96f88511008564ed03df0c6f6
aron211 <aronsandoval16@gmail.com>
2023-02-15 15:51:43 -0400

    add library toastr for messages delete an edit ready

--- src/app/vistas/edit/edit.component.ts
+++ src/app/vistas/edit/edit.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute} from '@angular/router'
 import { ApiService } from '../../services/api/api.service'
 import { pacienteI } from '../../modelos/paciente.interface'
+import { AlertasService } from '../../services/alertas/alertas.service'
+import { ResponseI } from '../../modelos/response.interface'
 import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
@@ -12,7 +14,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 })
 export class EditComponent implements OnInit {
 
-  constructor(private api:ApiService, private activerouter:ActivatedRoute, private router:Router) { }
+  constructor(private api:ApiService, private activerouter:ActivatedRoute, private router:Router, private alertas:AlertasService ) { }
 
   datosPacientes:pacienteI | undefined;
   editForm = new FormGroup ({
@@ -61,15 +63,29 @@ export class EditComponent implements OnInit {
   postForm(form: any){
     this.api.putPatient(form).subscribe(data =>{
         console.log(data)
-        this.close();
+        let respuesta:ResponseI =data
+        if(respuesta.status=='ok'){
+          this.alertas.showSuccess('Paciente Actualizado Exitosamente', 'Exito!!')
+          this.close();
+        }else{
+          this.alertas.showError(respuesta.result.error_msg, 'Error')
+        }
+        // this.close();
     })
   }
   delete(){
     // console.log('eliminado')
     let datos:any = this.editForm.value;
     this.api.delete(datos).subscribe(data =>{
+      let respuesta:ResponseI =data
+      if(respuesta.status=='ok'){
+        this.alertas.showSuccess('Paciente Eliminado Exitosamente', 'Exito!!')
+        this.close();
+      }else{
+        this.alertas.showError(respuesta.result.error_msg, 'Error')
+      }
       console.log(data)
-      this.close();
+      
     });
   }
   close(){
